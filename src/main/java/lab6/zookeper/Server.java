a523294c662fabe6b4b8c7d4d50735f94e0536da
Evgeniy <maximov.evgen@gmail.com>
2022-02-14 23:23:50 +0300

    Scripted auto-commit on change (2022-02-14 23:23:50) by gitwatch.sh

--- src/main/java/lab6/zookeper/Server.java
+++ src/main/java/lab6/zookeper/Server.java
@@ -1,4 +1,10 @@
 package lab6.zookeper;
 
+import java.io.IOException;
+
 public class Server {
+
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+
+    }
 }
