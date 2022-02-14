ddd747c634edc3d7d43a543c43f11972aef6513f
Evgeniy <maximov.evgen@gmail.com>
2022-02-15 00:39:54 +0300

    Scripted auto-commit on change (2022-02-15 00:39:54) by gitwatch.sh

--- src/main/java/lab6_zookeeper/StorageActor.java
+++ src/main/java/lab6_zookeeper/StorageActor.java
@@ -4,9 +4,12 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 
 import java.util.List;
+import java.util.Random;
 
 public class StorageActor extends AbstractActor {
     private List<String> servers;
+    private Random random = new Random();
+
 
     public Receive createReceive() {
         return receiveBuilder()
