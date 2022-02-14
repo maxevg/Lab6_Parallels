bf0a606f741ae945d7dd42e357fba1877d172814
Evgeniy <maximov.evgen@gmail.com>
2022-02-15 00:26:16 +0300

    Scripted auto-commit on change (2022-02-15 00:26:16) by gitwatch.sh

--- src/main/java/lab6_zookeeper/Server.java
+++ src/main/java/lab6_zookeeper/Server.java
@@ -1,5 +1,8 @@
 package lab6_zookeeper;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.zookeeper.KeeperException;
 
@@ -9,5 +12,7 @@ public class Server {
 
     public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
         BasicConfigurator.configure();
+        ActorSystem system = ActorSystem.create("routes");
+        ActorRef storage = system.actorOf(Props.create(StorageActor.class));
     }
 }
