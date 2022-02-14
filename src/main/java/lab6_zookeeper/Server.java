afcbb3559bc9f639727dba401db5ea5b581df63e
Evgeniy <maximov.evgen@gmail.com>
2022-02-15 01:08:00 +0300

    Scripted auto-commit on change (2022-02-15 01:08:00) by gitwatch.sh

--- src/main/java/lab6_zookeeper/Server.java
+++ src/main/java/lab6_zookeeper/Server.java
@@ -1,10 +1,15 @@
 package lab6_zookeeper;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
@@ -29,7 +34,12 @@ public class Server {
         final Http http = Http.get(system);
         ZooServer server = new ZooServer(zoo, storage);
         server.createServer("127.0.0.1:2181", "8080");
+        final Flow<HttpRequest, HttpResponse, NotUsed> flow = createRoute(storage, http)
+                .flow(system, materializer);
 
+    }
+
+    private static Route createRoute(ActorRef storage, final Http http) {
 
     }
 }
