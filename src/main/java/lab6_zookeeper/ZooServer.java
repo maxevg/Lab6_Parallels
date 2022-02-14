ea815930ab917658850c026c72e7f200ec156d88
Evgeniy <maximov.evgen@gmail.com>
2022-02-15 00:45:30 +0300

    Scripted auto-commit on change (2022-02-15 00:45:30) by gitwatch.sh

--- src/main/java/lab6_zookeeper/ZooServer.java
+++ src/main/java/lab6_zookeeper/ZooServer.java
@@ -1,7 +1,14 @@
 package lab6_zookeeper;
 
+import akka.actor.ActorRef;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 public class ZooServer implements Watcher {
-    
+
+    public ZooServer(ZooKeeper zoo, ActorRef storage) throws KeeperException, InterruptedException {
+        this.zoo = zoo;
+        this.storage = storage;
+    }    
 }
