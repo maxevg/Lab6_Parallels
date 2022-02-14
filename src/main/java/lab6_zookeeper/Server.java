c2794f84903b72030673921ec63e878e1655216c
Evgeniy <maximov.evgen@gmail.com>
2022-02-15 00:25:39 +0300

    Scripted auto-commit on change (2022-02-15 00:25:39) by gitwatch.sh

--- src/main/java/lab6_zookeeper/Server.java
+++ src/main/java/lab6_zookeeper/Server.java
@@ -1,5 +1,6 @@
 package lab6_zookeeper;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.zookeeper.KeeperException;
 
 import java.io.IOException;
@@ -7,6 +8,6 @@ import java.io.IOException;
 public class Server {
 
     public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
-        
+        BasicConfigurator.configure();
     }
 }
