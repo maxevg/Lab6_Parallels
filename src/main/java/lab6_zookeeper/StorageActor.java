47046bb6a80f484a78a49cf900cdd0539fb91aa4
Evgeniy <maximov.evgen@gmail.com>
2022-02-15 00:31:45 +0300

    Scripted auto-commit on change (2022-02-15 00:31:45) by gitwatch.sh

--- src/main/java/lab6_zookeeper/StorageActor.java
+++ src/main/java/lab6_zookeeper/StorageActor.java
@@ -2,8 +2,11 @@ package lab6_zookeeper;
 
 import akka.actor.AbstractActor;
 
+import java.util.List;
+
 public class StorageActor extends AbstractActor {
-    
+    private List<String> servers;
+
     public Receive createReceive() {
         return receiveBuilder()
                 .match(StoreServer.class, mes -> this.servers = mes.getServers())
